79211935aa9f11a3ca099aacac316cc7a64072ce
monte-dev <badziobw@gmail.com>
2023-12-30 20:57:00 +0100

    [Feat] Add 'not found' page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+	return (
+		<div className="bg-colors-dark-100 w-full h-screen flex flex-col items-center gap-y-4 justify-center text-white">
+			<h2 className="text-4xl font-bold pb-12">Not Found</h2>
+			<p className="pb-12">This page does not exist.</p>
+			<Link
+				href="/"
+				className="bg-colors-primary-400 border-colors-primary-200 border-2 rounded-md px-4 py-2 hover:bg-colors-primary-200 transition-all duration-150 ease-in"
+			>
+				Return Home
+			</Link>
+		</div>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,103 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px',
+			},
+		},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				colors: {
+					'primary-100': '#9b44c0',
+					'primary-200': '#8c36b4',
+					'primary-300': '#7d28a8',
+					'primary-400': '#6e199c',
+					'primary-500': '#5f0b90',
+
+					warning: '#a4161a',
+					white: '#f9f9f9ff',
+
+					'light-100': '#cfcfd2ff',
+					'light-200': '#a6a5abff',
+					'light-300': '#7c7b83ff',
+					'light-400': '#53515cff',
+					'light-500': '#292735ff',
+					'dark-100': '#292735',
+					'dark-200': '#23222e',
+					'dark-300': '#1e1d26',
+					'dark-400': '#18181f',
+					'dark-500': '#131317',
+					'dark-600': '#0d0e10',
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+			},
+		},
+	},
+	plugins: [require('tailwindcss-animate')],
+};
+export default config;
