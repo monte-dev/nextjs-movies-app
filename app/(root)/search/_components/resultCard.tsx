37540acabd535754fef9d25f91172ed1aa27bcf6
monte-dev <badziobw@gmail.com>
2024-01-04 23:57:05 +0100

    [Feat] Add tooltips and styling to resultCard

--- app/(root)/search/_components/resultCard.tsx
+++ app/(root)/search/_components/resultCard.tsx
@@ -1,3 +1,9 @@
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/components/ui/tooltip';
 import { Movies, Series } from '@/types/tmdb_types';
 
 interface ResultProps {
@@ -5,15 +11,15 @@ interface ResultProps {
 }
 const ResultCard = ({ result }: ResultProps) => {
 	return (
-		<div className="flex flex-col text-white justify-self-stretch rounded-md ">
+		<div className="flex flex-col text-white justify-self-stretch group">
 			<div className="">
 				<img
-					className="w-full"
+					className="w-full  rounded-lg group-hover:border-[1px] group-hover:border-colors-light300"
 					src={
 						process.env.NEXT_PUBLIC_TMDB_IMAGE_PATH
 							? process.env.NEXT_PUBLIC_TMDB_IMAGE_PATH +
 							  result.poster_path
-							: 'no-image.png'
+							: `/no-image.jpg`
 					}
 					alt={
 						result.hasOwnProperty('original_title')
@@ -22,12 +28,23 @@ const ResultCard = ({ result }: ResultProps) => {
 					}
 				/>
 			</div>
-			<h4 className="overflow-hidden">
-				{result.hasOwnProperty('original_title')
-					? (result as Movies).title
-					: (result as Series).name}
-			</h4>
-			<p>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger>
+						<h4 className="overflow-hidden text-sm text-left my-1">
+							{result.hasOwnProperty('original_title')
+								? (result as Movies).title.slice(0, 28) + '...'
+								: (result as Series).name.slice(0, 14) + '...'}
+						</h4>
+					</TooltipTrigger>
+					<TooltipContent>
+						{result.hasOwnProperty('original_title')
+							? (result as Movies).title
+							: (result as Series).name}
+					</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+			<p className="text-xs mt-1">
 				{result.hasOwnProperty('release_date')
 					? (result as Movies).release_date?.slice(0, 4)
 					: (result as Series).first_air_date?.slice(0, 4)}
--- app/(root)/search/_components/search-results.tsx
+++ app/(root)/search/_components/search-results.tsx
@@ -9,17 +9,27 @@ const SearchResults = async ({ query }: SearchResultsProps) => {
 	const movieResults = await getMoviesByQuery(query!);
 	const seriesResults = await getSeriesByQuery(query!);
 	return (
-		<div>
-			<div className="   py-8 pt-6 mx-12 grid grid-cols-6 items-start gap-2">
-				{movieResults.map((movie) => (
-					<ResultCard result={movie} key={movie.id} />
-				))}
-			</div>
-			<div className=" items-center justify-between py-8 pt-6 pb-20 grid grid-cols-6">
-				{seriesResults.map((series) => (
-					<ResultCard result={series} key={series.id} />
-				))}
-			</div>
+		<div className="py-8 pt-6 mx-12 text-white">
+			<section className="py-4">
+				<h3 className="my-2 w-1/4 mb-4 border-b-2 border-colors-primary-200 py-1">
+					Movies found:
+				</h3>
+				<div className=" grid grid-cols-6 items-start gap-x-3 gap-y-4">
+					{movieResults.map((movie) => (
+						<ResultCard result={movie} key={movie.id} />
+					))}
+				</div>
+			</section>
+			<section className="py-4">
+				<h3 className="my-2 w-1/4 mb-4 border-b-2 border-colors-primary-200 py-1">
+					Series found:
+				</h3>
+				<div className="grid grid-cols-6 items-start gap-x-3 gap-y-4">
+					{seriesResults.map((series) => (
+						<ResultCard result={series} key={series.id} />
+					))}
+				</div>
+			</section>
 		</div>
 	);
 };
